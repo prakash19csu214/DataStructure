a434215b7dd4d7132e06c6d76e9881b0fa817fda
Prakash Singh <70188303+prakash19csu214@users.noreply.github.com>
2020-08-28 00:10:24 +0530

    Create Q4.java

---
+++ Class Excercise 1/SET-A/Q4.java
@@ -0,0 +1,22 @@
+package com.ClassExercise1.SetA;
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n =s.nextInt();
+        int[]ar = new int[n];
+        for (int i = 0; i <ar.length ; i++) {
+          ar[i]=s.nextInt();
+        }
+        System.out.print("Enter element to be searched: ");
+        int search = s.nextInt();
+        int freq=0;
+        for (int i = 0; i <ar.length ; i++) {
+            if(ar[i]==search)
+                freq++;
+        }
+        System.out.println("Frequency: "+freq);
+    }
+}
